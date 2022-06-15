49b142b14c8758d3446de903c3c5e012ec9271c5
sabri-manai <60587331+sabri-manai@users.noreply.github.com>
2022-06-15 18:35:44 +0100

    Update app.js

--- app.js
+++ app.js
@@ -54,6 +54,4 @@ const products = require('./routes/product-routes')
 app.use('/products', products)
 
 
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+app.listen(process.env.PORT || 5000);
